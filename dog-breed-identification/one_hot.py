1c34303c3b12f0fef299eb441526224b2a663c3c
Sakif Mohammed <sakif@sm-ubuntu-18.04>
2020-03-23 20:36:21 -0230

    One-hot encoding for all dog breed classes.

--- dog-breed-identification/one_hot.py
+++ dog-breed-identification/one_hot.py
@@ -1,12 +1,16 @@
+import numpy as np
+
 # One hot maker
 
 naming_dict = {} # id: breed
 f = open("classes_t.csv", "r")
 fileContents = f.read()
 fileContents = fileContents.split('\n')
+one_hot = np.zeros(121, np.int8)
+one_hot[120] = 1
 
 for i in range(len(fileContents)-1):
   fileContents[i] = fileContents[i].split(',')
   naming_dict[fileContents[i][0]] = fileContents[i][0]
   inter = fileContents[i][0]
-  print('elif word_label == '+ inter +' : return np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])')
+  print('elif word_label == '+ inter +' : return np.' + repr(np.roll(one_hot, -i)).replace(', dtype=int8',''))
